cd4403d9769f1907e4542bd5cc0abdcb553bd3dc
Kaikyu <kaikyu.lotus@gmail.com>
2021-03-28 01:59:20 +0100

    Cleanup Github module Add Github enums

--- lib/src/modules/github/github_module.dart
+++ lib/src/modules/github/github_module.dart
@@ -11,21 +11,23 @@ import 'entities/exceptions/github_not_found_exception.dart';
 import 'entities/github_client.dart';
 import 'entities/github_events_response.dart';
 
-void eventsIsolateLoop(SendPort sendPort) {
-  // TODO maybe it would be better to use async and await?
+Future eventsIsolateLoop(SendPort sendPort) async {
   var db = KyaruDB();
   final githubClient = GithubClient();
   final etagStore = <String?, String?>{};
   final readUpdates = <String>[];
 
-  void elaborateResponse(DBRepo repo, GithubEventsResponse githubEventsResp) {
+  Future elaborateResponse(
+    DBRepo repo,
+    GithubEventsResponse githubEventsResp,
+  ) async {
     print('Elaborating events for ${repo.repo}...');
-    var events =
-        githubEventsResp.events!.where((e) => !readUpdates.contains(e.id));
+    var events = githubEventsResp.events!.where(
+      (e) => !readUpdates.contains(e.id),
+    );
 
-    if (events.isEmpty) {
-      return;
-    }
+    if (events.isEmpty) return;
+
     if (etagStore.containsKey(repo.repo)) {
       var message = '';
       if (events.length == 1) {
@@ -41,70 +43,52 @@ void eventsIsolateLoop(SendPort sendPort) {
     print('Elaborating events for ${repo.repo} done');
   }
 
-  void timerFunction(t) {
-    print('Checking github updates...');
-    db.syncDb();
-    for (var repo in db.getRepos()) {
-      githubClient
-          .events(repo.user, repo.repo, etag: etagStore[repo.repo])
-          .then((response) {
-        elaborateResponse(repo, response);
-        print('Left rate limit: ${response.rateLimitRemaining}');
-      }).catchError(
-        (e) {
-          print('Repository or user not found');
-          sendPort.send(['notFound', repo.toJson()]);
-        },
-        test: (e) => e.runtimeType == GithubNotFoundException,
-      ).catchError(
-        (e) {
-          print(
-            'Nothing changed in the repo, left limit: ${e.rateLimitRemaining}',
-          );
-        },
-        test: (e) => e.runtimeType == GithubNotChangedException,
-      ).catchError(
-        (e, s) {
-          print('Critical error $e\n$s');
-        },
-        test: (e) => e.runtimeType != GithubForbiddenException,
+  Future analyzeRepo(DBRepo repo) async {
+    try {
+      var response = await githubClient.events(
+        repo.user,
+        repo.repo,
+        etag: etagStore[repo.repo],
       );
+      await elaborateResponse(repo, response);
+      print('Left rate limit: ${response.rateLimitRemaining}');
+    } on GithubNotFoundException {
+      print('Repository or user not found');
+      sendPort.send(['notFound', repo.toJson()]);
+    } on GithubNotChangedException catch (e) {
+      print('Nothing changed, left limit: ${e.rateLimitRemaining}');
+    } on GithubForbiddenException catch (e, s) {
+      print('Critical error $e\n$s');
+    } catch (e, s) {
+      print('Unknown exception in analyzeRepo: $e\n$s');
     }
   }
 
-  Function()? loopBootstrapperFoo;
-  loopBootstrapperFoo = () {
-    var repoFutures = db
-        .getRepos()
-        .map((repo) => githubClient
-                .events(repo.user, repo.repo, etag: etagStore[repo.repo])
-                .then((r) {
-              etagStore[repo.repo] = r.etag;
-              readUpdates.addAll(List<String>.from(r.events!.map((e) => e.id)));
-            }))
-        .toList();
+  void timerFunction(Timer? timer) {
+    print('Checking github updates...');
+    db.syncDb();
+    db.getRepos().forEach(analyzeRepo);
+  }
 
-    Future.wait(repoFutures)
-        .then((nothing) => {
-              timerFunction(null),
-              Timer.periodic(Duration(minutes: 2), timerFunction)
-            })
-        .catchError(
-      (e) {
-        var resetDateTime = DateTime.fromMillisecondsSinceEpoch(
-          e.rateLimitReset * 1000,
-        );
-        var seconds = resetDateTime.difference(DateTime.now()).inSeconds;
-        print(
-          'Stopping updates until ${resetDateTime.toIso8601String()} ($seconds seconds)',
-        );
-        Future.delayed(Duration(seconds: seconds), loopBootstrapperFoo);
-      },
-      test: (e) => e.runtimeType == GithubForbiddenException,
-    );
-  };
+  Future loopBootstrapperFoo() async {
+    try {
+      timerFunction(null);
+      Timer.periodic(Duration(minutes: 2), timerFunction);
+    } on GithubForbiddenException catch (e) {
+      var resetDateTime = DateTime.fromMillisecondsSinceEpoch(
+        e.rateLimitReset! * 1000,
+      );
+      var seconds = resetDateTime.difference(DateTime.now()).inSeconds;
+      print(
+        'Stopping updates until ${resetDateTime.toIso8601String()}'
+        ' ($seconds seconds)',
+      );
+      Future.delayed(Duration(seconds: seconds), loopBootstrapperFoo);
+    }
+  }
 
-  loopBootstrapperFoo();
+  print('Bootstrapping Github event isolate');
+  await loopBootstrapperFoo();
 }
 
 class GithubModule implements IModule {
@@ -139,58 +123,68 @@ class GithubModule implements IModule {
   @override
   bool isEnabled() => true;
 
+  Future onSendMessageEvent(int chatId, String message) {
+    return _kyaru.brain.bot.sendMessage(ChatID(chatId), message);
+  }
+
+  Future onRepoNotFoundEvent(DBRepo repo) {
+    _kyaru.brain.db.removeRepo(repo);
+    return _kyaru.brain.bot.sendMessage(
+      ChatID(repo.chatID!),
+      'Hi, it seems that i can\'t access ${repo.user}/${repo.repo}, '
+      'so I removed it',
+    );
+  }
+
+  void onSocketMessage(dynamic data) async {
+    try {
+      if (data[0] == 'sendMessage') {
+        await onSendMessageEvent(data[1], data[2]);
+      } else if (data[0] == 'notFound') {
+        var repo = DBRepo.fromJson(data[1]);
+        await onRepoNotFoundEvent(repo);
+      }
+    } catch (e, s) {
+      print('Error handling onSocketMessage: $e\n$s');
+    }
+  }
+
   void startEventsIsolate() {
     var receivePort = ReceivePort();
     Isolate.spawn(eventsIsolateLoop, receivePort.sendPort);
-    receivePort.listen((data) {
-      if (data[0] == 'sendMessage') {
-        int chatId = data[1];
-        String message = data[2];
-        _kyaru.brain.bot
-            .sendMessage(ChatID(chatId), message)
-            .catchError((e, s) {
-          print('$e\n$s');
-        });
-      } else if (data[0] == 'notFound') {
-        var repo = DBRepo.fromJson(data[1]);
-        _kyaru.brain.db.removeRepo(repo);
-        _kyaru.brain.bot
-            .sendMessage(
-                ChatID(repo.chatID!),
-                'Hi, it seems that i can\'t access ${repo.user}/${repo.repo}, '
-                'so I removed it')
-            .catchError((e, s) {
-          print('$e\n$s');
-        });
-        ;
-      }
-    });
+    receivePort.listen(onSocketMessage);
   }
 
   Future registerRepo(Update update, _) async {
     var args = update.message!.text!.split(' ')..removeAt(0);
 
     if (args.length < 2) {
-      return await _kyaru.reply(update,
-          'This command needs two parameters, a Github username and a repository name');
+      return await _kyaru.reply(
+        update,
+        'This command needs two parameters, '
+        'a Github username and a repository name',
+      );
     }
 
     var username = args[0];
     var repo = args[1];
+    var dbRepo = DBRepo(update.message!.chat.id, username, repo);
 
     try {
       await _githubClient.events(username, repo);
-      _kyaru.brain.db.addRepo(DBRepo(update.message!.chat.id, username, repo));
-      await _kyaru.reply(update,
-          'From now on i\'ll send updates on new events for this repository in this chat');
     } on GithubNotFoundException {
-      await _kyaru.reply(update, 'Repository or user not found');
+      return _kyaru.reply(update, 'Repository or user not found');
     } on GithubNotChangedException {
-      await _kyaru.reply(update, 'There are no new events for this repository');
-    } on Exception catch (e, s) {
-      print('Could not get repo events: $e\n$s');
-      await _kyaru.reply(update, 'Something went terribly wrong...');
+      return _kyaru.reply(
+          update, 'There are no new events for this repository');
     }
+
+    _kyaru.brain.db.addRepo(dbRepo);
+    await _kyaru.reply(
+      update,
+      'From now on i\'ll send updates on '
+      'new events for this repository in this chat',
+    );
   }
 
   Future removeRepo(Update update, _) async {
