35478c68b38242f64dfe1287984d6d176f8493be
ale183 <ale183.dev@gmail.com>
2021-04-06 18:55:37 +0200

    Added quotes by anime title and character name

--- lib/src/modules/quotes/entities/quotes_client.dart
+++ lib/src/modules/quotes/entities/quotes_client.dart
@@ -9,10 +9,25 @@ class QuotesClient {
 
   final _client = Client();
 
-  Future<AnimeQuote> getQuote() async {
+  Future<AnimeQuote> getRandomQuote() async {
     var response = await _client
         .get(Uri.https(baseUrl, '/api/random'))
         .timeout(Duration(seconds: 120));
     return AnimeQuote.fromJson(json.decode(response.body));
+  }
+
+  Future<Map> getQuote({
+    required String mode,
+    required Map<String, dynamic> parameters,
+  }) async {
+    var response = await _client
+        .get(Uri.https(baseUrl, '/api/quotes/$mode', parameters))
+        .timeout(Duration(seconds: 120));
+    var result = json.decode(response.body);
+    if (result is List) {
+      result.shuffle();
+      return result[0];
+    }
+    return result;
   }
 }
